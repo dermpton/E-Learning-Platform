93f3b192273391fb509e76c7aeaac7d52016c8e4
dermpton <tcbhebhe@gmail.com>
2025-02-22 01:36:14 +0200

    Root fixed

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose'); 
+const path = require('path');
+const bodyParser = require('body-parser');
+const { title } = require('process');
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+ 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname + '/views'));
+app.use(cors());
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res)=>{
+    res.render('index', {msg: 'E-Learning-Platform', title: 'Making Pancakes'});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(app.get('port'), ()=>{
+  console.log(`Server started on http://localhost:${app.get('port')} press ctrl + c to exit`);
 });
-
-module.exports = app;
